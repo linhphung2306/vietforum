1335ecbeb1bb8a884684660a35817ba90e9fc284
linhphung2306 <linhphung2306@gmail.com>
2026-06-10 14:40:31 +0700

    fix cloudinary config

--- vietforum/settings.py
+++ vietforum/settings.py
@@ -8,6 +8,9 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
+if DEBUG:
+    ALLOWED_HOSTS += ['127.0.0.1', 'localhost']
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
