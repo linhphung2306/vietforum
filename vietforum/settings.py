fed4b62939911e45e4f36fa69a95cfc26d42d372
linhphung2306 <linhphung2306@gmail.com>
2026-06-10 09:26:16 +0700

    fix email port 465 ssl

--- vietforum/settings.py
+++ vietforum/settings.py
@@ -87,8 +87,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Email SMTP Gmail
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp.gmail.com'
-EMAIL_PORT          = 587
-EMAIL_USE_TLS       = True
+EMAIL_PORT          = 465
+EMAIL_USE_SSL       = True
+EMAIL_USE_TLS       = False
 EMAIL_HOST_USER     = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL  = os.environ.get('EMAIL_HOST_USER')
